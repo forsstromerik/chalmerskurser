33c4c11996ef2b01612a6e679a9f24d7711fa667
Erik Forsström <erikforsstrom.kode@gmail.com>
2018-11-14 14:26:00 +0100

    Make node server run https primarily

--- backend/backend.js
+++ backend/backend.js
@@ -4,6 +4,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const https = require('https');
+const fs = require('fs');
 
 const courses = require('./courses');
 
@@ -13,6 +15,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/courses', courses);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-})
+try {
+  const privateKey = fs.readFileSync('/etc/letsencrypt/live/chalmerskurser.se/privkey.pem', 'utf8');
+  const certificate = fs.readFileSync('/etc/letsencrypt/live/chalmerskurser.se/cert.pem', 'utf8');
+  const ca = fs.readFileSync('/etc/letsencrypt/live/chalmerskurser.se/chain.pem', 'utf8');
+  
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+  };
+  
+  const httpsServer = https.createServer(credentials, app);
+  
+  httpsServer.listen(process.env.PORT, () => {
+    console.log(`HTTPS Server running on port ${process.env.PORT}`);
+  });
+} catch {
+  app.listen(3000, () => {
+    console.log(`HTTPS Server running on port 80`);
+  })
+}
